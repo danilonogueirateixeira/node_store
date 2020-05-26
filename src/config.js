557c61acd82638495ae7419dd48836b99a562ac0
Danilo Nogueira <danilonogueirateixeira@gmail.com>
2020-05-25 22:21:29 -0300

    Config File

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = '';
+global.SALT_KEY = 'adfsdf23-4323-f345-dwer-12d34d45g5h6';
 global.EMAIL_TMPL = '<strong>{0}</strong>';
 
 module.exports = {
-    connectionString: '',
-    sendgridKey: '',
-    containerConnectionString: ''
+    connectionString: 'mongodb+srv://danilo_dnt:Uchiha22@cluster0-8lo4f.mongodb.net/test?retryWrites=true&w=majority',
+    sendgridKey: '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',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=danilonogueira;AccountKey=9CG/ldEoxEwtIx0P02871cBmeRg86rkun8YSyZBwIZZMwTGGt7WYAwvcTR25HAxt8W6SBnLOtU5qBmpS5gobvw==;EndpointSuffix=core.windows.net'
 }
